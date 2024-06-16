183121ceb922b17c8dd04d16395bad42516bde5a
jibinjames <jibinjames158@gmail.com>
2024-06-16 23:39:12 +0530

    [FEATURE] - Initial scaffold for API

---
+++ API/micro-services/marketmindspro/src/main/java/com/futurecoders/marketmindspro/MarketmindsproApplication.java
@@ -0,0 +1,13 @@
+package com.futurecoders.marketmindspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketmindsproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketmindsproApplication.class, args);
+	}
+
+}
---
+++ API/micro-services/marketmindspro/src/test/java/com/futurecoders/marketmindspro/MarketmindsproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurecoders.marketmindspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketmindsproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
